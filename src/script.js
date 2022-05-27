126b3aba4c3f320b875827c2ff20a816e07cecea
Jacqueline Xu <85534903+jqjacq@users.noreply.github.com>
2022-05-26 23:56:31 -0400

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,96 @@
+let history = document.getElementById("history")
+let about = document.getElementById("about")
+let benefits = document.getElementById("benefits")
+let common = document.getElementById("common")
+let sequence = document.getElementById("sequence")
+let reference = document.getElementById("reference")
+
+history.innerHTML = `
+<h1> History of Yoga</h1>
+
+Yoga’s history has many places of obscurity and uncertainty due to its oral transmission of sacred texts and the secretive nature of its teachings. The early writings on yoga were transcribed on fragile palm leaves that were easily damaged, destroyed or lost. The development of yoga can be traced back to over 5,000 years ago, but some researchers think that yoga may be up to 10,000 years old old. Yoga’s long rich history can be divided into four main periods of innovation, practice and development.
+<br><br>
+Pre-Classical Yoga:
+The beginnings of Yoga were developed by the Indus-Sarasvati civilization in Northern India over 5,000 years ago. The word yoga was first mentioned in the oldest sacred texts, the Rig Veda. The Vedas were a collection of texts containing songs, mantras and rituals to be used by Brahmans, the Vedic priests. Yoga was slowly refined and developed by the Brahmans and Rishis (mystic seers) who documented their practices and beliefs in the upanishads, a huge work containing over 200 scriptures. The most renowned of the Yogic scriptures is the Bhagavad-Gîtâ, composed around 500 B.C.E. The Upanishads took the idea of ritual sacrifice from the Vedas and internalized it, teaching the sacrifice of the ego through self-knowledge, action (karma yoga) and wisdom (jnana yoga).
+<br><br>
+Classical Yoga:
+In the pre-classical stage, yoga was a mishmash of various ideas, beliefs and techniques that often conflicted and contradicted each other. The Classical period is defined by Patanjali’s Yoga-Sûtras, the first systematic presentation of yoga. Written some time in the second century, this text describes the path of RAJA YOGA, often called "classical yoga". Patanjali organized the practice of yoga into an "eight limbed path" containing the steps and stages towards obtaining Samadhi or enlightenment. Patanjali is often considered the father of yoga and his Yoga-Sûtras still strongly influence most styles of modern yoga.
+<br><br>
+Post-Classical Yoga:
+A few centuries after Patanjali, yoga masters created a system of practices designed to rejuvenate the body and prolong life. They rejected the teachings of the ancient Vedas and embraced the physical body as the means to achieve enlightenment. They developed Tantra Yoga, with radical techniques to cleanse the body and mind to break the knots that bind us to our physical existence. This exploration of these physical-spiritual connections and body centered practices led to the creation of what we primarily think of yoga in the West: Hatha Yoga.
+<br><br>
+Modern Period:
+In the late 1800s and early 1900s, yoga masters began to travel to the West, attracting attention and followers. This began at the 1893 Parliament of Religions in Chicago, when Swami Vivekananda wowed the attendees with his lectures on yoga and the universality of the world’s religions. In the 1920s and 30s, Hatha Yoga was strongly promoted in India with the work of T. Krishnamacharya, Swami Sivananda and other yogis practicing Hatha Yoga. Krishnamacharya opened the first Hatha Yoga school in Mysore in 1924 and in 1936 Sivananda founded the Divine Life Society on the banks of the holy Ganges River. Krishnamacharya produced three students that would continue his legacy and increase the popularity of Hatha Yoga: B.K.S. Iyengar, T.K.V. Desikachar and Pattabhi Jois. Sivananda was a prolific author, writing over 200 books on yoga, and established nine ashrams and numerous yoga centers located around the world.
+<br><br>
+Written by Timothy Burgin - yogabasics.com
+`
+about.innerHTML = `
+<h1> What is Yoga?</h1>
+
+The literal translation of the word “yoga” from Sanskrit (which is recognized as the original language of yoga) is “union,” says Sherwin. (2) And that’s an apt basis for defining yoga as we know it today, she explains. “We define it as bringing together the mind and the body by use of the breath.”
+<br><br>
+Many people have come to know the physical benefits of yoga and think of it as a type of workout, says Sherwin. But yoga is way more than that. “It’s really an entire lifestyle; the postures are only one piece of it,” she says.
+<br><br>
+Though yoga, traditionally, is a system to foster well-being on the physical, mental, emotional, and spiritual levels, it is not generally thought of as a set of religious beliefs. (3) Yoga can be practiced in a completely secular manner, and it's practiced by people of all faith traditions, as well as people who are agnostic and atheist, according to the Yoga Alliance. (4)
+<br><br>
+Yoga is thought to have first come to the United States at the end of the 19th century and beginning of the 20th. One critical moment was when Swami Vivekananda spoke at the World Parliament of Religions in Chicago in 1893. (5) Another milestone in yoga’s spread in the west was Paramahansa Yogananda’s book, Autobiography of a Yogi, published in 1946 and still read by many yoga students, Sherwin says. (5) In the first half of the 20th century, it was more common for yoga instructors to travel from city to city to teach a yoga class or give a yoga lecture rather than teach in a studio (as is the case today). (5)
+<br><br>
+Notably, changes in U.S. immigration policy starting in 1965 allowed more South Asian immigrants to come to America (including yogis from India, where the practice was more established). (6) By the 1970s, yoga studios and books could be found more widely across the United States.
+<br><br>
+Written by Becky Upham - everydayhealth.com
+
+`
+benefits.innerHTML = `
+<h1>Benefit of Yoga</h1>
+Yoga is a great way to work on your flexibility and strength. Just about everyone can do it, too -- it's not just for people who can touch their toes or want to meditate. 
+<br><br>
+Some types of yoga are about relaxation. In others, you move more. Most types focus on learning poses, called asanas. They also usually include attention to breathing.
+<br><br>
+Yoga for Flexibility<br>
+Yoga poses work by stretching your muscles. They can help you move better and feel less stiff or tired.
+
+At any level of yoga, you'll probably start to notice benefits soon. In one study, people improved their flexibility by up to 35% after only 8 weeks of yoga.
+<br><br>
+Strike a Pose for Strength<br>
+Some styles of yoga, such as ashtanga and power yoga, are very physical. Practicing one of these styles will help you improve muscle tone.
+
+But even less vigorous styles of yoga, such as Iyengar or hatha, can provide strength and endurance benefits.
+
+Many of the poses, such as downward dog, upward dog, and the plank pose, build upper-body strength. The standing poses, especially if you hold them for several long breaths, build strength in your hamstrings, quadriceps, and abs. Poses that strengthen the lower back include upward dog and the chair pose.
+
+When done right, nearly all poses build core strength in the deep abdominal muscles.
+<br><br>
+
+Better Posture From Yoga<br>
+When you're stronger and more flexible, your posture improves.
+
+Most standing and sitting poses develop core strength, since you need your core muscles to support and maintain each pose.
+
+With a stronger core, you're more likely to sit and stand "tall."
+
+Yoga also helps your body awareness. That helps you notice more quickly if you're slouching or slumping, so you can adjust your posture.
+<br><br>
+Breathing Benefits <br>
+Yoga usually involves paying attention to your breath, which can help you relax. It may also call for specific breathing techniques.
+
+But yoga typically isn't aerobic, like running or cycling, unless it's an intense type of yoga.
+<br><br>
+Less Stress, More Calm <br>
+You may feel less stressed and more relaxed after doing some yoga.
+
+Some yoga styles use meditation techniques that help calm the mind. Focusing on your breathing during yoga can do that, too.
+
+Good for Your Heart
+Yoga has long been known to lower blood pressure and slow the heart rate. A slower heart rate can benefit people with high blood pressure or heart disease, and people who've had a stroke.
+
+Yoga has also been linked to lower cholesterol and triglyceride levels, and better immune system function.
+<br><br>
+Written by Rebecca Buffum Taylor - webmd.com
+`
+sequences.innerHTML = `
+a
+
+`
+reference.innerHTML = `
+a
+`
